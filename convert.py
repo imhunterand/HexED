ed08b5cf10f49d8daf0cccc20a66f813ff441318
Thomas ANDRI Wijayanto <hostbugbounty1337@gmail.com>
2023-11-24 12:01:46 +0700

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,33 @@
+# github.com/imhunterand
+print(""" 
+    ███╗██╗  ██╗███████╗██╗  ██╗███╗    ███████╗    ██╗██████╗ 
+    ██╔╝██║  ██║██╔════╝╚██╗██╔╝╚██║    ██╔════╝   ██╔╝██╔══██╗
+    ██║ ███████║█████╗   ╚███╔╝  ██║    █████╗    ██╔╝ ██║  ██║
+    ██║ ██╔══██║██╔══╝   ██╔██╗  ██║    ██╔══╝   ██╔╝  ██║  ██║
+    ███╗██║  ██║███████╗██╔╝ ██╗███║    ███████╗██╔╝   ██████╔╝
+    ╚══╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══╝    ╚══════╝╚═╝    ╚═════╝  
+    •   ┓               ┓   
+    ┓┏┳┓┣┓┓┏┏┓╋┏┓┏┓┏┓┏┓┏┫   @Security Executions Code (Pwn0sec)
+    ┗┛┗┗┛┗┗┻┛┗┗┗ ┛ ┗┻┛┗┗┻   @Imhunterand
+                                                                
+    """)
+def menu():
+    print(" 1. Hex to Text")
+    print(" 2. Text to Hex")
+    print(" 3. Exit")
+    pilih = int(input(" Select number : "))
+    if pilih == 1:
+        print("-"*20)
+        from converter import hex2txt
+    elif pilih == 2:
+        print("-"*20)
+        from converter import txt2hex
+    elif pilih == 3:
+        print("Bye")
+        exit()
+    else:
+        print("="*20)
+        print("\033[31m   Wrong input !!\033[39m")
+        print("="*20)
+        menu()
+menu()
